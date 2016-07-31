3afde431ba839eb0d291bc5522dd1bba1f6db690
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2016-07-31 14:44:37 +0200

    Improved parser for xml comments

--- src/XMLOffsets.hs
+++ src/XMLOffsets.hs
@@ -1,6 +1,15 @@
 module XMLOffsets
-  ( xmlNode
+  ( parseString
+  , parseFile
   , xmlDocument
+  , XML
+  , elementName
+  , elementAttributes
+  , elementOpenTagPosition
+  , elementCloseTagPosition
+  , elementContent
+  , textContent
+  , xmlSpanning
   ) where
 
 -- A simple xml parser that prints the positions of tags.  Usage:
@@ -40,13 +49,44 @@ data XML =  Element { name :: String
           | SpaceNode
         deriving (Show)
 
-openTag :: Parsec String u String
+elementName :: XML -> String
+elementName (Element n _ _ _ _ _ _) = n
+elementName (EmptyElement n _ _ _) = n
+
+elementAttributes :: XML -> [Attribute]
+elementAttributes (Element _ attrs _ _ _ _ _) = attrs
+elementAttributes (EmptyElement _ attrs _ _) = attrs
+
+xmlSpanning :: XML -> (Position, Position)
+xmlSpanning (Element _ _ s _ _ e _) = (s, e)
+xmlSpanning (EmptyElement _ _ s e) = (s, e)
+xmlSpanning (TextNode _ s e) = (s, e)
+xmlSpanning (Comment _ s e) = (s, e)
+xmlSpanning (XMLDecl _ s e) = (s, e)
+
+elementOpenTagPosition :: XML -> (Position, Position)
+elementOpenTagPosition (Element _ _ s e _ _ _) = (s, e)
+elementOpenTagPosition (EmptyElement _ _ s e) = (s, e)
+
+elementCloseTagPosition :: XML -> (Position, Position)
+elementCloseTagPosition (Element _ _ _ _ s e _) = (s, e)
+elementCloseTagPosition (EmptyElement _ _ s e) = (s, e)
+
+elementContent :: XML -> [XML]
+elementContent (Element _ _ _ _ _ _ c) = c
+
+textContent :: XML -> String
+textContent (TextNode t _ _) = t
+
+
+openTag :: Parsec String [Int] (String, [Attribute])
 openTag = do
   char '<'
-  elName <- many alphaNum
+  elName <- many1 alphaNum
+  attrs <- many attributeNode
   spaces
   char '>'
-  return elName
+  return (elName, attrs)
 
 closeTag :: String -> Parsec String [Int] Char
 closeTag elName = do
@@ -58,30 +98,30 @@ closeTag elName = do
 elementNode :: Parsec String [Int] XML
 elementNode = do
   openStartPos <- getOffset 0
-  elName <- openTag
+  nameAndAttrs <- openTag
   openEndPos <- getOffset (-1)
   inner <- many xmlNode
   closeStartPos <- getOffset 0
-  closeTag elName
+  closeTag (fst nameAndAttrs)
   closeEndPos <- getOffset (-1)
   -- Note: (-1) for the position of tags' ends, because the ending
   -- character was consumed by the parser. Follows, that when ever we
   -- calculate the length of a tag, it is _EndPos - _StartPos + 1
-  return $ Element elName [] openStartPos openEndPos closeStartPos closeEndPos inner
+  return $ Element (fst nameAndAttrs) (snd nameAndAttrs) openStartPos openEndPos closeStartPos closeEndPos inner
 
 emptyElementNode :: Parsec String [Int] XML
 emptyElementNode = do
   startPos <- getOffset 0
   char '<'
   elName <- many1 alphaNum
-  attrs <- many attribute
+  attrs <- many attributeNode
   spaces
   string "/>"
   endPos <- getOffset (-1)
   return $ EmptyElement elName attrs startPos endPos
 
-attribute :: Parsec String [Int] Attribute
-attribute = do
+attributeNode :: Parsec String [Int] Attribute
+attributeNode = do
   spaces
   attrName <- many1 (noneOf "= />")  
   spaces
@@ -104,7 +144,7 @@ comment :: Parsec String [Int] XML
 comment = do
   startPos <- getOffset 0
   string "<!--"
-  c <- manyTill anyChar (string "-->")
+  c <- manyTill anyChar (try (string "-->"))
   endPos <- getOffset (-1)
   return $ Comment ("<!--"++c++"-->") startPos endPos
 
@@ -124,14 +164,35 @@ xmlNode = try emptyElementNode <|> try elementNode <|> try textNode <|> try comm
 xmlDocument :: Parsec String [Int] XML
 xmlDocument = do
   skipMany space
-  --try xmlDecl
+  --try xmlDecl -- FIXME: parse optional xml declaration
   skipMany space
   tree <- try emptyElementNode <|> try elementNode
   skipMany space
   return $ tree
 
+
+parseString :: String -> String -> Either ParseError XML
+parseString doc fname =
+  runParser xmlDocument (lineOffsets' doc) fname doc 
+
+
+parseFile :: FilePath -> IO (Either ParseError XML)
+parseFile fname = do
+  c <- readFile fname
+  return $ runParser xmlDocument (lineOffsets' c) fname c
+
+
 main :: IO ()
 main = do
+  c <- getContents
+  case parseString c "(stdin)" of
+    Left e -> do putStrLn "Error parsing input:"
+                 print e
+    Right r -> print r
+
+
+main'' :: IO ()
+main'' = do
   c <- getContents
   case runParser xmlDocument (lineOffsets' c) "(stdin)" c of
     Left e -> do putStrLn "Error parsing input:"
