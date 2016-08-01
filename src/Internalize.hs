afa4024a650c7f337fb4fcd9a81659bd78971f62
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2016-08-02 00:57:47 +0200

    Internalizer: internalize and insertTags

--- src/Internalize.hs
+++ src/Internalize.hs
@@ -4,6 +4,11 @@ import XMLData
 import AnnotationData
 import LineOffsets
 
+
+-- Internalize
+internalize :: String -> [XML] -> [Annotation] -> (Int -> Annotation -> String) -> String
+internalize doc xs as serializer = insertTags (resolveOverlapping (flatten xs) as) serializer doc 0
+
 -- Split Annotations depending on internalized XML. 
 resolveOverlapping :: [XML] -> [Annotation] -> [Annotation]
 resolveOverlapping []  as = as
@@ -21,7 +26,7 @@ resolveOverlapping (x:xs) (a:as)
   | aStart < xStartOpen && aEnd < xEndClose = (fst leftSplit) : (resolveOverlapping (x:xs) ((snd leftSplit):as))
   -- a right-overlaps x
   | aStart < xStartOpen && aEnd < xEndClose = resolveOverlapping (x:xs) ((fst rightSplit):(snd rightSplit):as)
-  | otherwise = error "Did I miss something?"
+  | otherwise = error "Could not resolve overlapping!"
   where aStart = rangeStartOffset a
         aEnd = rangeEndOffset a
         xStartOpen = (posOffset (fst (xmlSpanning x)))
@@ -40,3 +45,26 @@ splitAnnotationAtCloseTag :: Annotation -> XML -> (Annotation, Annotation)
 splitAnnotationAtCloseTag (MarkupRange rid eid typ s1 e1 _) (Element _ _ _ _ sc ec _) =
    ((MarkupRange rid eid typ s1 (posOffset sc) "")
    , (MarkupRange rid eid typ ((posOffset ec)-1) e1 ""))
+
+-- Returns a list made from the tree.
+flatten :: [XML] -> [XML]
+flatten [] = []
+flatten ((Element n a so se sc ec inner):xs) =
+  (Element n a so se sc ec []) : flatten inner ++ flatten xs
+flatten (x:xs) = x : flatten xs
+
+-- This actually does the job of internalizing.
+insertTags :: [Annotation] -> (Int -> Annotation -> String) -> String -> Int -> String
+insertTags as slize [] idx = concatMap (slize idx) $ filter (\a -> ((rangeStartOffset a) >= idx) || ((rangeEndOffset a) >= idx)) as
+insertTags as slize (x:xs) idx = (concatMap (slize idx) (filter (\a -> ((rangeStartOffset a) == idx) || ((rangeEndOffset a) == idx)) as)) ++ (x : insertTags as slize xs (idx+1))
+
+-- FIXME: The serializer function takes an Integer and an Annotation
+-- as arguments. The integer is an index, by which the serializer can
+-- decide, whether to return an opening or a closing tag. -- This is
+-- not good: 1) Makes it hard to write serializers. 2) There may be
+-- situations, when the index does not equal start and end offsets,
+-- e.g. see edge condition. All decision logic should be implemented
+-- in insertTags, and the serializer should be called with
+-- Start|Close|Empty instead of an integer index.
+
+
