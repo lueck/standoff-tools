3858c37c6490ac58610a7d4fe9561b2d4e5e0614
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2016-08-21 19:23:26 +0200

    Improved --help.

--- src/standoff.hs
+++ src/standoff.hs
@@ -13,8 +13,10 @@ import StandOff.XML.LineOffsets (lineOffsets, Position, posOffset)
 import StandOff.ELisp.DumpFile (elDump)
 import StandOff.Internalizer.Internalize (internalize)
 import StandOff.XML.TagSerializer
+import StandOff.XML.AttributeSerializer
 import StandOff.Data.Annotation (makeAttributiveRanges, isMarkupRangeP, isRelationP, isPredicateP)
 import StandOff.Data.XML (XML, isXMLDeclarationP, isElementP, xmlSpanning)
+import StandOff.Data.Tag (NSNameValueSerializer(..))
 
 data OutputFormat = Raw
   | Json
@@ -30,6 +32,7 @@ data AnnotationTypes = AllAnnotations
 data Serializer = Simple
   | RDF String
   | Namespace String
+  | TEI String
   deriving (Eq, Show)
 
 data Options = Options
@@ -54,16 +57,28 @@ allAnnotations_ :: Parser AnnotationTypes
 allAnnotations_ = flag' AllAnnotations (short 'a' <> long "all-annotations" <> help "Parse all annotation types dumped in the elisp dump file.")
 
 rangeAnnotations_ :: Parser AnnotationTypes
-rangeAnnotations_ = flag' Ranges (short 'r' <> long "range-annotations" <> help "Parse only range annotation types dumped in the elisp dump file.")
+rangeAnnotations_ =
+  flag' Ranges (short 'r' <>
+                long "ranges" <>
+                help "Filter the output, so that only markup ranges are returned.")
 
 relationAnnotations_ :: Parser AnnotationTypes
-relationAnnotations_ = flag' Relations (short 'l' <> long "relation-annotations" <> help "Parse only relation annotation types dumped in the elisp dump file.")
+relationAnnotations_ =
+  flag' Relations (short 'l' <>
+                   long "relations" <>
+                   help "Filter the output, so that relations are returned.")
 
 predicateAnnotations_ :: Parser AnnotationTypes
-predicateAnnotations_ = flag' Predicates (short 'r' <> long "predicate-annotations" <> help "Parse only predicate annotation types dumped in the elisp dump file.")
+predicateAnnotations_ =
+  flag' Predicates (short 'p' <>
+                    long "predicates" <>
+                    help "Filter the output, so that only literal predicates are returned.")
 
 butRangeAnnotations_ :: Parser AnnotationTypes
-butRangeAnnotations_ = flag' ButRanges (short 'R' <> long "but-range-annotations" <> help "Parse all non-range annotation types (i.e. relations and predicates) dumped in the elisp dump file.")
+butRangeAnnotations_ =
+  flag' ButRanges (short 'R' <>
+                   long "but-ranges" <>
+                   help "Filter the output, so that only non-range annotation types (i.e. relations and predicates) are returned.")
 
 annotationTypes_ :: Parser AnnotationTypes
 annotationTypes_ = rangeAnnotations_ <|> relationAnnotations_ <|> predicateAnnotations_ <|> butRangeAnnotations_
@@ -78,7 +93,7 @@ dumped_ = Dumped
   <*> argument str (metavar "FILE")
 
 serializer_ :: Parser Serializer
-serializer_ = simpleSerializer_ <|> namespaceSerializer_ <|> rdfSerializer_
+serializer_ = simpleSerializer_ <|> namespaceSerializer_ <|> rdfSerializer_ <|> teiSerializer_
 
 simpleSerializer_ :: Parser Serializer
 simpleSerializer_ = flag' Simple (short 's' <> long "simple" <> help "Simple serializer only usefull for tags without namespaces.")
@@ -95,6 +110,12 @@ rdfSerializer_ =
                       <> long "rdf"
                       <> help "Serialize markup tags into elements of type ELEMENT writing the markup type as an \'rdf:a\'-attribute.")
 
+teiSerializer_ :: Parser Serializer
+teiSerializer_ =
+  TEI <$> strOption ( short 't'
+                      <> long "tei"
+                      <> help "Serialize markup tags into elements of type ELEMENT writing the markup type (local name) as a rendition-attribute.")
+
 internalize_ :: Parser Command
 internalize_ = Internalize
   <$> serializer_
@@ -102,19 +123,25 @@ internalize_ = Internalize
                              <> long "processing-instruction"
                              <> help "Insert a processing instruction into the result."))
   <*> argument str (metavar "DUMPFILE")
-  <*> argument str (metavar "XMLFILE")
+  <*> argument str (metavar "SOURCE")
   
 command_ :: Parser Command
 command_ = subparser
-  ( command "offsets"
-    (info offsets_
-      (progDesc "Parse line offsets"))
-    <> command "dumped"
-    (info dumped_
-      (progDesc "Parse emacs lisp dump file"))
+  ( command "dumped"
+    (info (dumped_ <**> helper)
+      (fullDesc <>
+       progDesc "Reads annotations generated with GNU Emacs' standoff-mode and dumped in FILE. The output can be formatted in raw (default) or in JSON format. The output can be filtered for annotation types. If no filtering option is given, all annotation types are returned in the output." <>
+       header "standoff dumped - a parser for annotations dumped in an emacs lisp file."))
     <> command "internalize"
-    (info internalize_
-      (progDesc "Internalize external markup"))
+    (info (internalize_ <**> helper)
+      (fullDesc <>
+       progDesc "Internalize external annotations given in DUMPFILE into SOURCE. DUMPFILE must be generated (or must look like it's been generated) with GNU Emacs' standoff-mode. SOURCE must be a valid XML file, at least it must contain a root node. There are options on how the internalizer should serialize markup ranges, its type information, IDs etc. By default only markup ranges are internalized, but not relations." <>
+       header "standoff internalize - internalize standoff markup into an xml file."))
+    <> command "offsets"
+    (info (offsets_ <**> helper)
+      (fullDesc <>
+       progDesc "Returns the character offsets, lines and columns of the nodes of an XML file." <>
+       header "standoff offsets - an xml parser returning node positions."))
   )
 
 parseGeneric :: (Monad m, P.Stream s Identity t) => String -> P.Parsec s u a -> u -> P.SourceName -> s -> m a
@@ -158,19 +185,30 @@ run (Internalize slizer procInstr dumpFile xmlFile) = do
            procInstr
            (behindXMLDeclOrTop xml))
   where slizer' = case slizer of
-                    Simple -> serializeTag
+                    Simple -> serializeTag serializeAttributes'
                     -- FIXME: define tag serializers for attributes and options
-                    RDF elName -> serializeSpanTag serializeAttributes elName
-                    Namespace prefix -> serializeNsTag serializeAttributes prefix
+                    RDF elName -> serializeSpanTag serializeAttributes' elName
+                    Namespace prefix -> serializeNsTag serializeAttributes' prefix
+                    TEI elName -> serializeSpanTag teiAttributes elName 
         insertAt s (Just new) pos = (take pos s) ++ "\n" ++ new ++ (drop (pos) s)
         insertAt s Nothing _ = s
         behindXMLDeclOrTop xml
           | length decl == 1 = (posOffset $ snd $ xmlSpanning $ head decl) - 1
           | otherwise = 0
           where decl = filter isXMLDeclarationP xml
+        teiAttributes = (serializeAttributes
+                          (Just "id")
+                          (Just "eid")
+                          (Just "rendition")
+                          LocalName)
 
 opts :: ParserInfo Command
-opts = info (command_ <**> helper) idm
+opts = info
+       (command_ <**> helper)
+       (fullDesc <>
+         header "standoff - a tool for handling standoff annotations (aka external markup)." <>
+         progDesc "standoff offers commands for parsing a dump file that contains external markup and for internalizing such external markup into an xml file. There is also a command for parsing a file for newlines, which may be useful for debugging." <>
+         footer "See also: https://github.com/lueck/standoff-mode/ - standoff-mode is a tagger for GNU Emacs.")
 
 main :: IO ()
 main = execParser opts >>= run
