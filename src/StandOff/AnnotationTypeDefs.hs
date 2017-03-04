b44474bd7412837b0f9bce5e2359a9c5849a2ddb
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2017-03-04 02:40:44 +0100

    Better doc strings

--- src/StandOff/AnnotationTypeDefs.hs
+++ src/StandOff/AnnotationTypeDefs.hs
@@ -11,18 +11,24 @@ import Data.UUID.Aeson
 
 import StandOff.TextRange
 
-data Annotation = MarkupRange { rangeId :: Maybe UUID
-                              , elementId :: UUID
-                              , markupType :: String
-                              , startOffset :: Int
-                              , endOffset :: Int
-                              , text :: String
-                              , attributes :: Map.Map String [String] }
-                | Relation { relationId :: UUID
-                           , subject :: UUID
-                           , predicate :: String
-                           , object :: UUID }
-                deriving (Show, Generic)
+-- | Algebraic data type for annotations.
+data Annotation
+  = MarkupRange                  -- ^ range of characters in source file
+    { rangeId :: Maybe UUID      -- ^ the UUID of the range
+    , elementId :: UUID          -- ^ the UUID of the element the range is member of
+    , markupType :: String       -- ^ the annotation type
+    , startOffset :: Int         -- ^ the start character offset
+    , endOffset :: Int           -- ^ the end character offset
+    , text :: String             -- ^ the string between 'startOffset' and 'endOffset'
+    , attributes :: Map.Map String [String] } -- ^ attributes of the range
+  | Relation                     -- ^ a relation between two markup elements 
+    { relationId :: UUID         -- ^ the UUID of the relation
+    , subject :: UUID            -- ^ the UUID of the markup element
+                                 -- which is the relation's subject
+    , predicate :: String        -- ^ the relation's predicate
+    , object :: UUID }           -- ^ the UUID of the markup element
+                                 -- which is the relation's object
+  deriving (Show, Generic)
 
 instance TextRange (Annotation) where
   start (MarkupRange _ _ _ s _ _ _) = s
@@ -35,6 +41,26 @@ instance TextRange (Annotation) where
 instance ToJSON Annotation where
   toEncoding = genericToEncoding defaultOptions
 
+-- * Predicates to be used by filters.
+
+-- | Returns true for a 'MarkupRange'.
+isMarkupRangeP :: Annotation -> Bool
+isMarkupRangeP (MarkupRange _ _ _ _ _ _ _) = True
+isMarkupRangeP _ = False
+
+-- | Returns true for a 'Relation'.
+isRelationP :: Annotation -> Bool
+isRelationP (Relation _ _ _ _) = True
+isRelationP _ = False
+
+-- | Returns true for a Predicate.
+isPredicateP :: Annotation -> Bool
+isPredicateP _ = False
+
+
+-- * Getting the record fields of a 'MarkupRange'.
+--
+-- Patterns are not exhaustive. Filter before using!
 
 rangeRangeId :: Annotation -> Maybe UUID
 rangeRangeId (MarkupRange rid _ _ _ _ _ _) = rid
@@ -55,27 +81,15 @@ rangeAttributes :: Annotation -> Map.Map String [String]
 rangeAttributes (MarkupRange _ _ _ _ _ _ attrs) = attrs
 
 
--- Predicates to be used by filters.
+-- * Handling attributes
 
-isMarkupRangeP :: Annotation -> Bool
-isMarkupRangeP (MarkupRange _ _ _ _ _ _ _) = True
-isMarkupRangeP _ = False
-
-isRelationP :: Annotation -> Bool
-isRelationP (Relation _ _ _ _) = True
-isRelationP _ = False
-
-isPredicateP :: Annotation -> Bool
-isPredicateP _ = False
-
-
--- handling attributes
-
+-- | Insert an attribute to a markup range's attribute list.
 insertAttributeWith :: Annotation -> (String, String) -> Annotation
 insertAttributeWith (MarkupRange rid eid typ s e txt attrs) (k, v) =
   (MarkupRange rid eid typ s e txt (Map.insertWith (++) k [v] attrs))
 insertAttributeWith a _ = a
 
+-- | This actually only filters markup ranges.
 makeAttributiveRanges :: [Annotation] -> [Annotation]
 makeAttributiveRanges as = ranges
   where ranges = filter isMarkupRangeP as
