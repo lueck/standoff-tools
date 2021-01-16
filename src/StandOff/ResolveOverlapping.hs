048967980bacd1014f83291164ede02cfb57ae7c
Christian Lück <christian.lueck@uni-muenster.de>
2021-01-16 12:33:44 +0100

    use new semantics for resolvings overlaps
    - tests fail (3)

--- src/StandOff/ResolveOverlapping.hs
+++ src/StandOff/ResolveOverlapping.hs
@@ -37,12 +37,12 @@ merge :: (Tree b, TextRange b, TextRange a) => [b] -> a -> [a]
 merge [] a = [a]
 merge (x:xs) a
   -- Split a when a right-overlaps x.
-  | -- a `rightOverlaps` x =
-    start a > start x && end a >= end x && start a < end x =
+  | a `rightOverlaps` x =
+    -- start a > start x && end a >= end x && start a < end x =
     (merge (contents x) (fst rightSplit')) ++ (merge xs (snd rightSplit'))
   -- Split a when a left-overlaps x.
-  | -- a `leftOverlaps` x =
-    start a <= start x && end a < end x && end a > start x =
+  | a `leftOverlaps` x =
+    -- start a <= start x && end a < end x && end a > start x =
     (fst leftSplit') : (merge (x:xs) (snd leftSplit')) -- (merge (contents x) (snd leftSplit'))
   -- Forward xml vertically when x contains a
   | x `contains` a = merge (contents x) a
@@ -50,10 +50,10 @@ merge (x:xs) a
   | a `behind` x = (merge xs a)
   -- Needn't progress if a contains x, because then xs are not
   -- relevant and a contains the content of x, too.
-  | a `contains` x = merge xs a
-  --  | a `spansEq` x = [a]
-  --  | a `contains` x && a `startsBefore` x = (fst leftSplit') : (merge (x:xs) (snd leftSplit'))
-  --  | a `contains` x && a `endsBehind` x = (merge (contents x) (fst rightSplit')) ++ (merge xs (snd rightSplit'))
+  --  | a `contains` x = merge xs a
+  | a `spansEq` x = [a]
+  | a `contains` x && a `startsBefore` x = (fst leftSplit') : (merge (x:xs) (snd leftSplit'))
+  | a `contains` x && a `endsBehind` x = (merge (contents x) (fst rightSplit')) ++ (merge xs (snd rightSplit'))
   -- Needn't progress behind a.
   | a `before` x = [a]
   | otherwise = error "Could not resolve overlapping!"
