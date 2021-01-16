f85ff78bd87c053373a57ee2595257558e5e35f6
Christian Lück <christian.lueck@uni-muenster.de>
2021-01-16 22:00:19 +0100

    documentation

--- src/StandOff/MarkupTree.hs
+++ src/StandOff/MarkupTree.hs
@@ -3,7 +3,8 @@ module StandOff.MarkupTree
   )
 where
 
--- | An n-ary tree with markup, i.e. a rose tree like XML.
+-- | An n-ary tree with markup, i.e. a rose tree like XML. The type
+-- for representing internal markup must be an instance of this class.
 class MarkupTree a where
   -- | Return the children of a tree node, which are markup
   -- elements. This should not return text nodes.
