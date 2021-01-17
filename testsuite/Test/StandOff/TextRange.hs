319d620d3b472a42de37e69c33ab67649da12eb7
Christian Lück <christian.lueck@uni-muenster.de>
2021-01-17 20:34:36 +0100

    make unittests more readable

--- testsuite/Test/StandOff/TextRange.hs
+++ testsuite/Test/StandOff/TextRange.hs
@@ -28,118 +28,97 @@ internal = [ (elm "root" 1 1000
               ])
            ]
 
+-- | create a test case from start and end position, then merge it
+-- with 'internal' and return the spans.
+mergeCase s e = map spans $ merge internal (mRng "a" "a" "a" s e)
+
 -- test left-overlapping
 test_mergeLeftOverlapping = do
-  assertEqual 2 (length resolved)
-  assertEqual [(260, 269), (274, 282)] (map spans resolved)
-  where resolved = merge internal (mRng "a" "a" "a" 260 282)
+  assertEqual [(260, 269), (274, 282)] (mergeCase 260 282)
 
 -- test split on right-overlapping and no-split on contain
 test_mergeRightOverlapping = do
-  assertEqual 2 (length resolved)
-  assertEqual [(235, 235), (241, 260)] (map spans resolved)
-  where resolved = merge internal (mRng "a" "a" "a" 235 260)
+  assertEqual [(235, 235), (241, 260)] (mergeCase 235 260)
 
 -- test two splits on left- and right-overlapping
 test_mergeLeftRightOverlapping = do
-  assertEqual 3 (length resolved)
-  assertEqual [(235, 235), (241,269), (274, 282)] (map spans resolved)
-  where resolved = merge internal (mRng "a" "a" "a" 235 282)
+  assertEqual [(235, 235), (241,269), (274, 282)] (mergeCase 235 282)
 
 -- test lost opening tag
 test_mergeLostOpeningTag = do
-  assertEqual [(234, 235)] (map spans resolved)
-  where resolved = merge internal (mRng "a" "a" "a" 233 240)
+  assertEqual [(234, 235)] (mergeCase 233 240)
 
 -- test lost closing tag
 test_mergeLostClosingTag = do
-  assertEqual [(234, 235)] (map spans resolved)
-  where resolved = merge internal (mRng "a" "a" "a" 230 235)
+  assertEqual [(234, 235)] (mergeCase 230 235)
 
 -- test in opening tag
 test_mergeInOpeningTag = do
-  assertEqual [(234, 235)] (map spans resolved)
-  where resolved = merge internal (mRng "a" "a" "a" 231 240)
+  assertEqual [(234, 235)] (mergeCase 231 240)
 
 -- test in closing tag
 test_mergeInClosingTag = do
-  assertEqual [(234, 235)] (map spans resolved)
-  where resolved = merge internal (mRng "a" "a" "a" 230 239)
+  assertEqual [(234, 235)] (mergeCase 230 239)
 
 -- test in both tags
 test_mergeInBothTags = do
-  assertEqual [(234, 235)] (map spans resolved)
-  where resolved = merge internal (mRng "a" "a" "a" 231 239)
+  assertEqual [(234, 235)] (mergeCase 231 239)
 
 -- test merge into region without markup
 test_mergePlain = do
-  assertEqual [(210,220)] (map spans resolved)
-  where resolved = merge internal (mRng "a" "a" "a" 210 220)
+  assertEqual [(210,220)] (mergeCase 210 220)
 
 -- test merge containing
 test_mergeContaining = do
-  assertEqual [(220,250)] (map spans resolved)
-  where resolved = merge internal (mRng "a" "a" "a" 220 250)
+  assertEqual [(220,250)] (mergeCase 220 250)
 
 -- test merge with equal spans
 test_mergeSpanEqual = do
-  assertEqual [(230,240)] (map spans resolved)
-  where resolved = merge internal (mRng "a" "a" "a" 230 240)
+  assertEqual [(230,240)] (mergeCase 230 240)
 
 -- test merge contained (same as plain?)
 test_mergeContained = do
-  assertEqual [(150,160)] (map spans resolved)
-  where resolved = merge internal (mRng "a" "a" "a" 150 160)
+  assertEqual [(150,160)] (mergeCase 150 160)
 
 -- test merge behind
 test_mergeBehind = do
-  assertEqual [(2100,2200)] (map spans resolved)
-  where resolved = merge internal (mRng "a" "a" "a" 2100 2200)
+  assertEqual [(2100,2200)] (mergeCase 2100 2200)
 
 -- test merge before
 test_mergeBefore = do
-  assertEqual [(0,0)] (map spans resolved)
-  where resolved = merge internal (mRng "a" "a" "a" 0 0)
+  assertEqual [(0,0)] (mergeCase 0 0)
 
 -- test merge before at
 test_mergeNextBefore = do
-  assertEqual [(0,1)] (map spans resolved)
-  where resolved = merge internal (mRng "a" "a" "a" 0 1)
+  assertEqual [(0,1)] (mergeCase 0 1)
 
 -- test end extends into open tag
 test_mergeEndIntoOpening = do
-  assertEqual [(210,229)] (map spans resolved)
-  where resolved = merge internal (mRng "a" "a" "a" 210 231)
+  assertEqual [(210,229)] (mergeCase 210 231)
 
 -- test end extends into closing tag (same as test_mergeInClosingTag)
 test_mergeEndIntoClosing = do
-  assertEqual [(234,235)] (map spans resolved)
-  where resolved = merge internal (mRng "a" "a" "a" 234 239)
+  assertEqual [(234,235)] (mergeCase 234 239)
 
 -- test start extends into open tag (same as test_mergeInOpeningTag)
 test_mergeStartIntoOpening = do
-  assertEqual [(234,235)] (map spans resolved)
-  where resolved = merge internal (mRng "a" "a" "a" 231 235)
+  assertEqual [(234,235)] (mergeCase 231 235)
 
 -- test start extends into closing tag
 test_mergeStartIntoClosing = do
-  assertEqual [(241,245)] (map spans resolved)
-  where resolved = merge internal (mRng "a" "a" "a" 239 245)
+  assertEqual [(241,245)] (mergeCase 239 245)
 
 -- test markup in a single opening tag
 test_mergeInSingleOpening = do
-  assertEqual [] (map spans resolved)
-  where resolved = merge internal (mRng "a" "a" "a" 231 232)
+  assertEqual [] (mergeCase 231 232)
 
 -- test markup in a single closing tag
 test_mergeInSingleClosing = do
-  assertEqual [] (map spans resolved)
-  where resolved = merge internal (mRng "a" "a" "a" 238 239)
+  assertEqual [] (mergeCase 238 239)
 
 -- test markup between to elements, extending into both of them
 test_mergeExtendsIntoClosingOpening = do
-  assertEqual [(241,269)] (map spans resolved)
-  where resolved = merge internal (mRng "a" "a" "a" 239 271)
+  assertEqual [(241,269)] (mergeCase 239 271)
 
 
 -- corner case: test markup in a single closing tag
