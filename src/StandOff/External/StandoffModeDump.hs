3823f59931eda31cf4159757d0bda6045a2a0abb
Christian Lück <christian.lueck@uni-muenster.de>
2021-08-11 11:57:14 +0200

    rewrite of module for parsing standoff-mode annotations

--- src/StandOff/External/StandoffModeDump.hs
+++ src/StandOff/External/StandoffModeDump.hs
@@ -1,11 +1,93 @@
+{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
+
 module StandOff.External.StandoffModeDump
+  ( StandoffModeRange(..)
+  , StandoffModeAnnotations(..)
+  , elDump
+  , runELispDumpParser
+  )
   where
 
-import Text.Parsec
-import qualified Data.Map as Map
-import Data.UUID.Types hiding (nil)
+-- | This module provides functions for parsing external annotations
+-- that where generated in Emacs' standoff-mode.
 
-import StandOff.AnnotationTypeDefs
+import GHC.Generics
+import Data.Time
+import Data.UUID.Types hiding (nil)
+import qualified Data.Aeson as A
+import Data.Aeson ((.:))
+import qualified Data.Aeson.Types as A
+import Control.Applicative ((<|>))
+import Text.Read (readMaybe)
+import Text.Parsec hiding ((<|>))
+
+import StandOff.TextRange
+
+
+-- * Data type
+
+-- | A record for markup ranges from Emacs' standoff-mode.
+data StandoffModeRange
+  = StandoffModeRange             -- ^ range of characters in source file
+    { somr_id :: Maybe UUID       -- ^ the UUID of the range
+    , somr_elementId :: UUID      -- ^ the UUID of the element to which the range belongs
+    , somr_type :: String         -- ^ the annotation type
+    , somr_start :: Int           -- ^ the start character offset
+    , somr_end :: Int             -- ^ the end character offset
+    , somr_createdAt :: Maybe UTCTime -- ^ a unix timestamp
+    , somr_createdBy :: Maybe String -- ^ the annotator's user name
+    }
+  deriving (Show)
+
+instance TextRange (StandoffModeRange) where
+  start = somr_start
+  end = somr_end
+  split _ range (e1, s2) = (range {somr_end = e1}, range {somr_start = s2})
+
+
+-- | A record for representing the contents of standoff-mode's
+-- external markup. Note: This is incomplete, since we parse
+-- interesting stuff only.
+data StandoffModeAnnotations
+  = StandoffModeAnnotations
+  { som_md5sum :: String
+  , som_ranges :: [StandoffModeRange]
+  }
+  deriving (Show, Generic)
+
+
+-- * Parsing JSON files
+
+instance A.FromJSON StandoffModeRange where
+  parseJSON (A.Object v) = StandoffModeRange
+    <$> v .: "markupRangeId"
+    <*> v .: "markupElementId"
+    <*> v .: "qualifiedName"
+    <*> ((v .: "sourceStart") <|> (fmap (assertInt . readMaybe) $ v .: "sourceStart"))
+    <*> ((v .: "sourceEnd") <|> (fmap (assertInt . readMaybe) $ v .: "sourceEnd"))
+    <*> (fmap readFormattedTime $ v .: "createdAt")
+    <*> v .: "createdBy"
+    where
+      assertInt Nothing = 0
+      assertInt (Just i) = i
+  parseJSON invalid = A.typeMismatch "Object" invalid
+
+-- | Parse the formatted time in "createdAt"
+readFormattedTime :: String -> Maybe UTCTime
+readFormattedTime s = parseTimeM True defaultTimeLocale "%Y-%m-%dT%H:%M:%S%Z" s
+
+
+instance A.FromJSON StandoffModeAnnotations where
+  parseJSON = A.genericParseJSON A.defaultOptions { A.fieldLabelModifier = somJsonFields }
+    where
+      somJsonFields :: String -> String
+      somJsonFields "som_md5sum" = "md5sum"
+      somJsonFields "som_ranges" = "MarkupRanges"
+      somJsonFields s = s
+
+
+-- * Parsing elisp dump files
+
 
 uuid :: Parsec String () UUID
 uuid = do
@@ -41,14 +123,14 @@ quoteString = do
   char '"'
   return $ concat s
 
-nil :: Parsec String () [Annotation]
+nil :: Parsec String () [StandoffModeRange]
 nil = do
   spaces
   string "nil"
   spaces
   return []
 
-markupRange :: Parsec String () Annotation
+markupRange :: Parsec String () StandoffModeRange
 markupRange = do
   char '('
   spaces
@@ -68,19 +150,21 @@ markupRange = do
   spaces
   char ')'
   spaces
-  return $ MarkupRange { rangeId = rangeId
-                       , elementId = elemId
-                       , markupType = typ
-                       -- Emacs starts with col 1, so -1 for start offset.
-                       , startOffset = ((read start)::Int) - 1
-                       -- Emacs starts with col 1 and standoff-mode
-                       -- defines the ranges end at the following
-                       -- char, so -2 for end offset.
-                       , endOffset = ((read end)::Int) - 2
-                       , text = Nothing -- Just txt -- Drop the text
-                       , attributes = Map.empty }
+  return $ StandoffModeRange
+    { somr_id = rangeId
+    , somr_elementId = elemId
+    , somr_type = typ
+    -- Emacs starts with col 1, so -1 for start offset.
+    , somr_start = ((read start)::Int) - 1
+    -- Emacs starts with col 1 and standoff-mode
+    -- defines the ranges end at the following
+    -- char, so -2 for end offset.
+    , somr_end = ((read end)::Int) - 2
+    , somr_createdBy = Nothing -- FIXME
+    , somr_createdAt = Nothing -- FIXME
+    }
 
-markupRanges :: Parsec String () [Annotation]
+markupRanges :: Parsec String () [StandoffModeRange]
 markupRanges = do
   spaces
   char '('
@@ -90,7 +174,7 @@ markupRanges = do
   char ')'
   return rs
 
-relation :: Parsec String () Annotation
+relation :: Parsec String () ()
 relation = do
   char '('
   spaces
@@ -108,26 +192,18 @@ relation = do
   spaces
   char ')'
   spaces
-  case (secondUuid) of
-    Nothing -> return $ Relation { relationId = Nothing
-                                 , subject = firstUuid
-                                 , predicate = predicat
-                                 , object = obj }
-    Just subj -> return $ Relation { relationId = Just firstUuid
-                                   , subject = subj
-                                   , predicate = predicat
-                                   , object = obj }
+  return ()
 
-relations :: Parsec String () [Annotation]
+relations :: Parsec String () [StandoffModeRange]
 relations = do
   spaces
   char '('
   rels <- many relation
   char ')'
   spaces
-  return rels
+  return []
 
-elDump :: Parsec String () [Annotation]
+elDump :: Parsec String () [StandoffModeRange]
 elDump = do
   spaces
   string "(setq standoff-markup-read-function-dumped (quote"
@@ -142,7 +218,7 @@ elDump = do
   return $ ranges++rels
 
 -- | Run the dump parser in the IO monad.
-runELispDumpParser :: SourceName -> String -> IO [Annotation]
+runELispDumpParser :: SourceName -> String -> IO [StandoffModeRange]
 runELispDumpParser location contents = do
   return $ either (fail . (err++) . show) id $ parse elDump location contents
   where
@@ -157,4 +233,7 @@ main :: IO ()
 main = do
   c <- getContents
   rc <- runELispDumpParser "stdin" c
-  print c
+  -- needs import qualified Data.ByteString.Lazy as BS
+  -- c <- BS.getContents
+  -- let rc = A.decode c :: Maybe StandoffModeAnnotations
+  print rc
