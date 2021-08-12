f7f4afd514b0ec36b5211ea4cf04249a863d98ba
Christian Lück <christian.lueck@uni-muenster.de>
2021-08-12 09:58:36 +0200

    tried to use a GADT to abstract away types of external markup

--- src/StandOff/External.hs
+++ src/StandOff/External.hs
@@ -1,9 +1,13 @@
+{-# LANGUAGE GADTs #-}
 module StandOff.External
 where
 
 -- | Functions common to external markup.
 
 import qualified Data.Map as Map
+
+import StandOff.TextRange
+
 
 type ExternalAttributes = Map.Map String String
 
@@ -16,3 +20,18 @@ class IdentifiableSplit e where
   markupId :: e -> Maybe String
   splitNum :: e -> Maybe Int
   updSplitNum :: e -> Int -> e
+
+
+-- | A GADT that can be wrapped around types of external markup which
+-- is an instance of 'TextRange', 'ToAttributes', and
+-- 'IdentifiableSplit'.
+data ExternalMarkup
+  where
+    MkExternalMarkup :: (TextRange a, ToAttributes a, IdentifiableSplit a) => a -> ExternalMarkup
+
+instance TextRange ExternalMarkup where
+  start (MkExternalMarkup e) = start e
+  end (MkExternalMarkup e) = end e
+  spans (MkExternalMarkup e) = spans e
+  split typ (MkExternalMarkup e) (x,y) = split typ e (x,y) -- > We cannot do that! :(
+  splitPoints (MkExternalMarkup e) = splitPoints e
