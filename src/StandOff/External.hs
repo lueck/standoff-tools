e8b4d1e4069747dfb7672f7a12366987e2e312f5
Christian Lück <christian.lueck@uni-muenster.de>
2021-08-12 08:04:21 +0200

    introduce class IdentifiableSplit

--- src/StandOff/External.hs
+++ src/StandOff/External.hs
@@ -10,3 +10,9 @@ type ExternalAttributes = Map.Map String String
 -- | External markup should provide a map of features or attributes.
 class ToAttributes e where
   attributes :: e -> ExternalAttributes
+
+-- | External markup should provide means for identification.
+class IdentifiableSplit e where
+  markupId :: e -> Maybe String
+  splitNum :: e -> Maybe Int
+  updSplitNum :: e -> Int -> e
