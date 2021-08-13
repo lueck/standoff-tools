c8ccc993c867b1c3080baf365a343782f22d11af
Christian Lück <christian.lueck@uni-muenster.de>
2021-08-13 13:54:04 +0200

    manage GADT problem by introducing a generic markup type

--- src/StandOff/External.hs
+++ src/StandOff/External.hs
@@ -1,5 +1,11 @@
 {-# LANGUAGE GADTs #-}
 module StandOff.External
+  ( ToAttributes(..)
+  , IdentifiableSplit(..)
+  , ExternalMarkup(..)
+  , GenericMarkup
+  , genMrkp
+  )
 where
 
 -- | Functions common to external markup.
@@ -22,6 +28,11 @@ class IdentifiableSplit e where
   updSplitNum :: e -> Int -> e
 
 
+-- * Generic
+
+-- | Generic types for simplifiying apps like the CLI of this package.
+
+
 -- | A GADT that can be wrapped around types of external markup which
 -- is an instance of 'TextRange', 'ToAttributes', and
 -- 'IdentifiableSplit'.
@@ -33,5 +44,43 @@ instance TextRange ExternalMarkup where
   start (MkExternalMarkup e) = start e
   end (MkExternalMarkup e) = end e
   spans (MkExternalMarkup e) = spans e
-  split typ (MkExternalMarkup e) (x,y) = split typ e (x,y) -- > We cannot do that! :(
+  -- split typ (MkExternalMarkup e) (x,y) = split typ (genMrkp e) (x,y) -- We cannot do that! :(
+  -- We cannot create new GADTs from a GADT, so we need make a new ADT and rewrap it!
+  split typ (MkExternalMarkup e) (x,y) =
+    (,) <$> MkExternalMarkup . fst <*> MkExternalMarkup . snd $
+    split typ (genMrkp e) (x,y)
   splitPoints (MkExternalMarkup e) = splitPoints e
+
+
+-- ** A generic record
+
+data GenericMarkup
+  = GenericMarkup
+  { genmrkp_start :: Int
+  , genmrkp_end :: Int
+  , genmrkp_features :: Map.Map String String
+  -- | For internal use:
+  , genmrkp_splitNum :: Maybe Int  -- ^ number of the split
+  } deriving (Show)
+
+
+instance TextRange GenericMarkup where
+  start = genmrkp_start
+  end = genmrkp_end
+  split _ range (e1, s2) = (range {genmrkp_end = e1}, range {genmrkp_start = s2})
+
+instance ToAttributes GenericMarkup where
+  attributes = genmrkp_features
+
+instance IdentifiableSplit GenericMarkup where
+  markupId = Map.lookup "id" . genmrkp_features
+  splitNum = genmrkp_splitNum
+  updSplitNum r i = r { genmrkp_splitNum = Just i }
+
+
+genMrkp :: (TextRange a, ToAttributes a, IdentifiableSplit a) => a -> GenericMarkup
+genMrkp = GenericMarkup
+  <$> start
+  <*> end
+  <*> attributes
+  <*> splitNum
