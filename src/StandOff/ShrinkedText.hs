dbacff641f0463132d8a17b8242a6bb07eecf87a
Christian Lück <christian.lueck@uni-muenster.de>
2022-08-08 23:31:09 +0200

    added new modules XTraverse and ShrinkedText

--- src/StandOff/ShrinkedText.hs
+++ src/StandOff/ShrinkedText.hs
@@ -1,40 +1,61 @@
-module ShrinkedText
+{-# LANGUAGE DatatypeContexts #-}
+{-# LANGUAGE TupleSections #-}
+module StandOff.ShrinkedText
 where
 
-import Control.Monad.Writer
-import Control.Monad.Reader
 import Control.Monad.State
-import qualified Data.Map.Strict as Map
+import Data.Map.Strict (Map)
 import Data.Tree.Class
 
-
 import StandOff.LineOffsets
-import StandOff.StringLike
+import StandOff.StringLike (StringLike)
 import StandOff.XTraverse
 
 
 -- | A mapping of offsets in the XML file to offsets in the generated
 -- plain text file. The list index represents the XML offset, the
 -- element value the offset in the plain text file.
-data OffsetMapping = OffsetMapping
-  { offsetMap_offsetMapping :: [Int] -- ^ mapping of character offsets
-  , offsetMap_byteMapping :: [Int]   -- ^ mapping of byte offsets
-  }
+type OffsetMapping = [Int]
+
+-- data OffsetMapping = OffsetMapping
+--   { offsetMap_offsetMapping :: [Int] -- ^ mapping of character offsets
+--   , offsetMap_byteMapping :: [Int]   -- ^ mapping of byte offsets
+--   }
 
 initialOffsetMapping :: OffsetMapping
-initialOffsetMapping = OffsetMapping [] []
+-- initialOffsetMapping = OffsetMapping [] []
+initialOffsetMapping = []
 
+-- | A record for the configuration that determines shrinking of
+-- all kinds of nodes.
+data (StringLike s) => ShrinkingNodeConfig s = ShrinkingNodeConfig
+  { _shrinkCfg_tagReplacements :: (ShrinkingNodeReplacements s)
+  , _shrinkCfg_defaultTagReplacement :: ShrinkingNodeReplacement s
+  , _shrinkCfg_defaultPiReplacement :: s
+  -- replacing entities should be done by the parser!?
+  , _shrinkCfg_entityReplacements :: EntityResolver s
+  , _shrinkCfg_defaultEntityReplacements :: s
+  }
 
--- | A config of node qnames to replacement strings
-type ShrinkingNodeConfig = Map.Map String String
+-- | A config of node qnames to replacement strings.
+type ShrinkingNodeReplacements s = Map String (ShrinkingNodeReplacement s)
+-- TODO: replace String key type with QName
 
+data (StringLike s) => ShrinkingNodeReplacement s = ShrinkingNodeReplacement
+  { _shrinkRepl_open :: s
+  , _shrinkRepl_close :: s
+  , _shrinkRepl_empty :: s
+  }
+
+-- | A mapping of entity names to resolved strings.
+type EntityResolver s = Map String s
 
 class ShrinkingNode n where
   -- | This processes an opening part of a node (in case of an element
   -- node) or the complete node
   shrinkOpen
     :: (StringLike s) =>
-    ShrinkingNodeConfig -- ^ a configuration
+       ShrinkingNodeConfig s -- ^ a configuration
     -> n                -- ^ the node
     -> s                -- ^ the input string from this node till EOF
     -> OffsetMapping    -- ^ the the offset mapping generated so far
@@ -46,7 +67,7 @@ class ShrinkingNode n where
   -- the same as in 'serializeOpen'.
   shrinkClose
     :: (StringLike s) =>
-       ShrinkingNodeConfig
+       ShrinkingNodeConfig s
     -> n
     -> s
     -> OffsetMapping
@@ -54,15 +75,35 @@ class ShrinkingNode n where
 
 
 -- | Generate shrinked text.
+shrinkedText'
+  :: (StringLike s, Monoid s, Monad m, Tree t, ShrinkingNode n) =>
+     (s -> m ())  -- ^ monadic action
+  -> ShrinkingNodeConfig s
+  -> [t n]        -- ^ the parsed XML document
+  -> s            -- ^ the XML document as a string
+  -> m (OffsetMapping)
+shrinkedText' writeM nodeCfg xml s = do
+  final <- execStateT (mapM_ (xtraverse_ writeM f g) xml) (s, initialOffsetMapping)
+  -- We cannot write what is left over, because we do not have a
+  -- mapping for it. Should we do a test on the final state or write
+  -- what is left over?
+  --
+  -- writeM $ fst final
+  return $ snd final
+  where
+    f n = uncurry (shrinkOpen nodeCfg n)
+    g n = uncurry (shrinkClose nodeCfg n)
+
+
 shrinkedText
   :: (StringLike s, Monoid s, Monad m, Tree t, ShrinkingNode n) =>
      (s -> m ())  -- ^ monadic action
-  -> ShrinkingNodeConfig
+  -> ShrinkingNodeConfig s
   -> [t n]        -- ^ the parsed XML document
   -> s            -- ^ the XML document as a string
   -> m (OffsetMapping)
 shrinkedText writeM nodeCfg xml s = do
-  final <- execStateT (mapM_ (xtraverse_ writeM f g) xml) (s, initialOffsetMapping)
+  final <- xtraverseWithState writeM f g xml (s, initialOffsetMapping)
   -- We cannot write what is left over, because we do not have a
   -- mapping for it. Should we do a test on the final state or write
   -- what is left over?
--- src/StandOff/XTraverse.hs
+++ src/StandOff/XTraverse.hs
@@ -2,8 +2,10 @@ module StandOff.XTraverse
 where
 
 import Data.Tree.Class
+import Control.Monad.Trans
 import Control.Monad.State
 
+-- * Traversing an XML tree with state
 
 -- | An in-order traversal with one function applied on the open tag
 -- and an other function applied the close tag.
@@ -30,3 +32,36 @@ xtraverse_ write_ f g xml = do
   where
     node = getNode xml
 
+-- | Execute a stateful in-order traversal on a 'Tree'. This applies
+-- two state manipulating functions f and g on each node, f on the
+-- open tag, g on the close tag. What f and g return is passed to a
+-- monadic action, e.g. written to IO. The final state is returned.
+xtraverseWithState
+  :: (Monad m, Tree t) =>
+     (a -> m ())        -- ^ monadic action for writing the result of a tag function
+  -> (n -> s -> (a, s)) -- ^ the state manipulating function applied on the open tag
+  -> (n -> s -> (a, s)) -- ^ the state manipulating function applied on the close tag
+  -> [t n]              -- ^ the parsed XML document
+  -> s                  -- ^ initial state
+  -> m (s)
+xtraverseWithState writeM f g xml s = execStateT (mapM_ (xtraverse_ writeM f g) xml) s
+
+
+-- * More abstractions
+
+-- | An abstraction of 'xtraverse_' for arbitrary monad transformers.
+xtraverse'
+  :: (Monad m, MonadTrans tr, Monad (tr m), Tree t) =>
+     (a -> m c)        -- ^ monadic action on what a tag function returns
+  -> (b -> tr m a)     -- ^ function wrapped around tag function in the monad transformer
+  -> (n -> b)          -- ^ the function applied on the open tag
+  -> (n -> b)          -- ^ the function applied on the close tag
+  -> t n               -- ^ the parsed 'XMLTree'
+  -> tr m (c, c)
+xtraverse' write_ pipe f g xml = do
+  o <- pipe (f node) >>= (lift . write_)
+  mapM_ (xtraverse' write_ pipe f g) $ getChildren xml
+  c <- pipe (g node) >>= (lift . write_)
+  return (o, c)
+  where
+    node = getNode xml
