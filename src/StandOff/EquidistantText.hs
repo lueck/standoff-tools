bff8f65f23723ca7d44219f8cb2ae2566afa36ca
Christian Lück <christian.lueck@uni-muenster.de>
2022-08-12 18:41:14 +0200

    use state execution from StandOff.XTraverse, better documentation

--- src/StandOff/EquidistantText.hs
+++ src/StandOff/EquidistantText.hs
@@ -2,7 +2,6 @@ module StandOff.EquidistantText
 where
 
 import Data.Tree.Class
-import Control.Monad.State
 
 import StandOff.StringLike
 import StandOff.XTraverse
@@ -11,10 +10,31 @@ import StandOff.XTraverse
 -- | The first in the resulting tuple is the string to be printed for
 -- this node, the second is the new state.
 class EquidistantNode n where
-  serializeOpen :: (StringLike s) => Char -> n -> s -> (s, s)
+  -- | Make an equidistant representation of an opening tag. For XML
+  -- this is the replacement for the opening tag of an non-empty
+  -- element node, or a complete equidistant representation of any
+  -- other node.
+  serializeOpen
+    :: (StringLike s) =>
+       Char -- ^ the filling character
+    -> n    -- ^ the current node
+    -> s    -- ^ state: the portion of the input file not yet seen
+    -> (s, s) -- ^ returns the equidistant representation of the
+              -- current node and the new state, i.e. the portion of
+              -- the input file not yet processed
+  -- | Make an equidistant representation of a closing tag. For XML
+  -- this is the empty string for every type of node but a non-empty
+  -- element.
   serializeClose :: (StringLike s) => Char -> n -> s -> (s, s)
 
 
+-- | Generate equidistant text.
+--
+-- Implementation notice: This traverses the XML tree with a
+-- state. The state object is just the XML document as a string. The
+-- current state is the portion of the string not yet processed. Each
+-- node's equidistant string representation is written to a monad,
+-- e.g. IO.
 equidistantText
   :: (StringLike s, Monoid s, Monad m, Tree t, EquidistantNode n) =>
      (s -> m ())  -- ^ monadic action
@@ -22,10 +42,5 @@ equidistantText
   -> [t n]        -- ^ the parsed XML document
   -> s            -- ^ the XML document as a string
   -> m ()
-equidistantText writeM fillChar xml s = do
-  final <- execStateT (mapM_ (xtraverse_ writeM f g) xml) s
-  writeM final
-  -- Should we do a test on the final state or write what is left over?
-  where
-    f = serializeOpen fillChar
-    g = serializeClose fillChar
+equidistantText writeM fillChar xml s =
+  xtraverseWithState writeM (serializeOpen fillChar) (serializeClose fillChar) xml s >>= writeM
