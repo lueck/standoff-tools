8d56f1f1f01fe34679fa23b41a614e6dab52646b
Christian Lück <christian.lueck@uni-muenster.de>
2022-08-26 18:03:16 +0200

    unit test on equidistant text of advanced.xml with tab character

--- testsuite/Test/StandOff/EquidistantText.hs
+++ testsuite/Test/StandOff/EquidistantText.hs
@@ -7,6 +7,10 @@ import Data.Traversable
 import Data.Foldable
 import Control.Monad.Writer
 import Data.Tree.Class
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+import qualified Data.ByteString as BS
+import Text.Parsec.Text
 
 import StandOff.DomTypeDefs hiding (start, end, getNode)
 import StandOff.XmlParsec
@@ -30,7 +34,7 @@ test_equidistantTextFromSimple = do
   assertEqual expected txt
 
 test_equidistantTextFromAdvanced = do
-  unitTestPending "advanced.xml is not extracted correctly!"
+  unitTestPending "advanced.xml is not extracted correctly! Parsec updates positions on \\t characters differently."
   let fPath = "testsuite/advanced.xml"
   let txtPath = "testsuite/advanced.equidist.txt"
   c <- readFile fPath
@@ -40,3 +44,23 @@ test_equidistantTextFromAdvanced = do
   expected <- readFile txtPath
   assertEqual (length expected) (length txt)
   assertEqual expected txt
+
+-- | We to work around the problem of parsec and tabs, we replace
+-- every tab character on the input stream with a space.
+test_equidistantTextFromAdvancedTabsReplaced = do
+  unitTestPending "Something still wrong here"
+  let fPath = "testsuite/advanced.xml"
+  let txtPath = "testsuite/advanced.equidist.txt"
+  c <- BS.readFile fPath
+  let c' =  T.decodeUtf8 c
+  let notabs = T.map replaceTab c'
+  lOffsets <- LOFF.runLineOffsetParser (show fPath) notabs
+  xml <- runXmlParser lOffsets (show fPath) notabs
+  (_, txt) <- runWriterT (equidistantText tell ' ' xml $ T.unpack c')
+  expected <- readFile txtPath
+  assertEqual (length expected) (length txt)
+  assertEqual expected txt
+  where
+    replaceTab :: Char -> Char
+    replaceTab '\t' = ' '
+    replaceTab c = c
