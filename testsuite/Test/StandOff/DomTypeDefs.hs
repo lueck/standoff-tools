570e9f3a51ed8c83e732adfc28c4559b03e27101
Christian Lück <christian.lueck@uni-muenster.de>
2022-08-29 00:45:11 +0200

    added unit test for splitting around character and entity references

---
+++ testsuite/Test/StandOff/DomTypeDefs.hs
@@ -0,0 +1,102 @@
+{-# OPTIONS_GHC -F -pgmF htfpp #-}
+module Test.StandOff.DomTypeDefs (htf_thisModulesTests) where
+
+import Test.Framework
+import Data.Tree.Class (getNode)
+
+import StandOff.DomTypeDefs (XmlNode, XMLTrees)
+import StandOff.TextRange
+import StandOff.XmlParsec
+import StandOff.SourcePosMapping
+import StandOff.Splitting
+
+import Test.StandOff.TestSetup
+
+test_elementImplementsTextRange = do
+  assertEqual 100 (start d)
+  assertEqual 200 (end d)
+  assertEqual (100, 200) (spans d)
+  assertEqual ((99, 105), (194, 201)) (splitPoints d)
+  assertEqual 100 (len d)
+  assertThrowsSome (split FstSplit d)
+  assertThrowsSome (leftSplit FstSplit d (getNode $ elm "x" 120 350 []))
+  assertThrowsSome (rightSplit FstSplit d (getNode $ elm "x" 12 150 []))
+  assertEqual True (d <<>> (getNode $ elm "span" 120 150 []))
+  assertEqual True (d <<>> (getNode $ elm "span" 100 150 []))
+  assertEqual True (d <<>> (getNode $ elm "span" 120 200 []))
+  assertEqual False (d <<>> (getNode $ elm "span" 120 210 []))
+  assertEqual True (d `before` (getNode $ elm "div" 210 300 []))
+  assertEqual True (d `before` (getNode $ elm "div" 200 300 []))
+  assertEqual False (d `before` (getNode $ elm "div" 10 30 []))
+  assertEqual True (d `behind` (getNode $ elm "div" 10 30 []))
+  assertEqual True (d `behind` (getNode $ elm "div" 10 100 []))
+  assertEqual False (d `behind` (getNode $ elm "div" 210 300 []))
+  assertEqual True (d `leftOverlaps` (getNode $ elm "div" 190 300 []))
+  assertEqual False (d `leftOverlaps` (getNode $ elm "div" 90 130 []))
+  assertEqual True (d `rightOverlaps` (getNode $ elm "div" 90 130 []))
+  assertEqual False (d `rightOverlaps` (getNode $ elm "div" 210 300 []))
+  where
+    d :: XmlNode Int String String
+    d = getNode $ elm "div" 100 200 ([]::XMLTrees Int String String)
+
+
+mergeCase internal' s e = map spans $ merge internal' (mRng "a" "a" "a" s e)
+
+indexed = 0
+
+test_splitSimple = do
+  let fPath = "testsuite/simple.xml"
+  c <- readFile fPath
+  offsetMapping <- parsecOffsetMapping indexed (show fPath) c
+  xml <- runXmlParser offsetMapping (show fPath) c
+  assertEqual [(0x68, 0x72)] $ mergeCase xml 0x68 0x72
+  assertEqual [(0x68, 0x72)] $ mergeCase xml 0x65 0x72
+
+test_splitCharRefEndOnRef = do
+  let fPath = "testsuite/charref.xml"
+  c <- readFile fPath
+  offsetMapping <- parsecOffsetMapping indexed (show fPath) c
+  xml <- runXmlParser offsetMapping (show fPath) c
+  assertEqual [(0x0a, 0x1f)] $ mergeCase xml 0x0a 0x1f
+  assertEqual [(0x0a, 0x15)] $ mergeCase xml 0x0a 0x18
+  assertEqual [(0x0a, 0x15)] $ mergeCase xml 0x0a 0x19
+  assertEqual [(0x0a, 0x15)] $ mergeCase xml 0x0a 0x1a
+  assertEqual [(0x0a, 0x15)] $ mergeCase xml 0x0a 0x1b
+  assertEqual [(0x0a, 0x1c)] $ mergeCase xml 0x0a 0x1c
+
+test_splitCharRefStartOnRef = do
+  unitTestPending "something wrong when moved over charref border"
+  let fPath = "testsuite/charref.xml"
+  c <- readFile fPath
+  offsetMapping <- parsecOffsetMapping indexed (show fPath) c
+  xml <- runXmlParser offsetMapping (show fPath) c
+  assertEqual [(0x1c, 0x1f)] $ mergeCase xml 0x19 0x1f
+  assertEqual [(0x1c, 0x1f)] $ mergeCase xml 0x1a 0x1f
+  assertEqual [(0x1c, 0x1f)] $ mergeCase xml 0x1b 0x1f -- <<<
+  assertEqual [(0x1c, 0x1f)] $ mergeCase xml 0x1c 0x1f
+  assertEqual [(0x1d, 0x1f)] $ mergeCase xml 0x1d 0x1f
+
+test_splitEntityRefEndOnRef = do
+  unitTestPending "end on false position"
+  let fPath = "testsuite/entityref.xml"
+  c <- readFile fPath
+  offsetMapping <- parsecOffsetMapping indexed (show fPath) c
+  xml <- runXmlParser offsetMapping (show fPath) c
+  assertEqual [(0x29, 0x2c)] $ mergeCase xml 0x29 0x2e
+  assertEqual [(0x29, 0x2c)] $ mergeCase xml 0x29 0x2d
+  assertEqual [(0x29, 0x2c)] $ mergeCase xml 0x29 0x2c
+  assertEqual [(0x29, 0x2c)] $ mergeCase xml 0x29 0x2b
+  -- assertEqual [(0x29, 0x2c)] $ mergeCase xml 0x29 0x1f
+
+test_splitEntityRefStartOnRef = do
+  unitTestPending "something wrong when moved over charref border"
+  let fPath = "testsuite/charref.xml"
+  c <- readFile fPath
+  offsetMapping <- parsecOffsetMapping indexed (show fPath) c
+  xml <- runXmlParser offsetMapping (show fPath) c
+  assertEqual [(0x37, 0x3b)] $ mergeCase xml 0x33 0x3b
+  assertEqual [(0x37, 0x3b)] $ mergeCase xml 0x34 0x3b
+  assertEqual [(0x37, 0x3b)] $ mergeCase xml 0x35 0x3b
+  assertEqual [(0x37, 0x3b)] $ mergeCase xml 0x36 0x3b
+  assertEqual [(0x37, 0x3b)] $ mergeCase xml 0x37 0x3b
+  assertEqual [(0x38, 0x3b)] $ mergeCase xml 0x38 0x3b
--- testsuite/TestMain.hs
+++ testsuite/TestMain.hs
@@ -4,13 +4,13 @@ module Main where
 import Test.Framework
 import Test.Framework.BlackBoxTest
 import {-@ HTF_TESTS @-} Test.StandOff.MarkupRange
-import {-@ HTF_TESTS @-} Test.StandOff.DataXML
 import {-@ HTF_TESTS @-} Test.StandOff.TagSerializer
 import {-@ HTF_TESTS @-} Test.StandOff.TextRange
 import {-@ HTF_TESTS @-} Test.StandOff.XmlParsec
 import {-@ HTF_TESTS @-} Test.StandOff.EquidistantText
 import {-@ HTF_TESTS @-} Test.StandOff.SourcePosMapping
 import {-@ HTF_TESTS @-} Test.StandOff.MarkupTree
+import {-@ HTF_TESTS @-} Test.StandOff.DomTypeDefs
 -- import {-@ HTF_TESTS @-} Test.StandOff.External.StandoffModeDump
 
 main = htfMain htf_importedTests
